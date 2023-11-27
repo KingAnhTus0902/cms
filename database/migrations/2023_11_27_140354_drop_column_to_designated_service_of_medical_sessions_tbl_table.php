8b923257ecd5aae23c8ebfe4aaa15f9bbce4ed2c
Nguyen Anh Tu <nguyenanhtu.jvb@gmail.com>
2023-11-27 14:07:39 +0700

    drop column designated_service_of_medical_sessions_tbl

---
+++ database/migrations/2023_11_27_140354_drop_column_to_designated_service_of_medical_sessions_tbl_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('designated_service_of_medical_sessions_tbl', function (Blueprint $table) {
+            if (Schema::hasColumn('designated_service_of_medical_sessions_tbl', 'designated_service_insurance_code')) {
+                $table->dropColumn('designated_service_insurance_code');
+            }
+            if (Schema::hasColumn('designated_service_of_medical_sessions_tbl', 'designated_insurance_unit_price')) {
+                $table->dropColumn('designated_insurance_unit_price');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('designated_service_of_medical_sessions_tbl', function (Blueprint $table) {
+            //
+        });
+    }
+};
